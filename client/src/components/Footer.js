0b83d3e8c60a4c8f9eec93522b4d69c263d93bb6
Maurice <mauricenganga41@gmail.com>
2022-07-25 20:32:48 +0300

    Create a footer component to be used in the website

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,26 +1,26 @@
-// import styled from "styled-components";
+import styled from "styled-components";
 
-// const Footers = styled.footer`
-//   position: absolute;
-//   padding: 60px;
-//   text-align: center;
-//   bottom: 0;
-//   width: 100%;
-//   height: 2.5rem;
-//   box-sizing: border-box;
-// `;
+const Footers = styled.footer`
+  position: absolute;
+  padding: 60px;
+  text-align: center;
+  bottom: 0;
+  width: 100%;
+  height: 2.5rem;
+  box-sizing: border-box;
+`;
 
-// const Span = styled.span`
-//   color: #ccc;
-// `;
+const Span = styled.span`
+  color: ;
+`;
 
-// const Footer = () => {
-//   const year = new Date().getFullYear();
-//   return (
-//     <Footers>
-//       <Span>Copyright ⓒ {year}</Span>
-//     </Footers>
-//   );
-// };
+const Footer = () => {
+  const year = new Date().getFullYear();
+  return (
+    <Footers>
+      <Span>Copyright ⓒ {year}</Span>
+    </Footers>
+  );
+};
 
-// export default Footer;
+export default Footer;
