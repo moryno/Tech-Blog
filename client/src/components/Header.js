419c30a4fc487b0e77b07271a97164d760335b37
Maurice <mauricenganga41@gmail.com>
2022-07-25 18:24:14 +0300

    Modify th Header component by adding an image and title of the tech blog website

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,51 +1,51 @@
-// import styled from "styled-components";
+import styled from "styled-components";
 
-// const Container = styled.section`
-//   margin-top: 3.75rem;
-// `;
+const Container = styled.section`
+  margin-top: 3.75rem;
+`;
 
-// const TitleWrapper = styled.article`
-//   display: flex;
-//   flex-direction: column;
-//   align-items: center;
-//   font-family: "Lora", serif;
-//   color: #444;
-// `;
+const TitleWrapper = styled.article`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-family: "Lora", serif;
+  color: #444;
+`;
 
-// const Title = styled.span`
-//   position: absolute;
-//   top: 12%;
-//   font-size: 6.25rem;
-// `;
+const Title = styled.span`
+  position: absolute;
+  top: 12%;
+  font-size: 6.25rem;
+`;
 
-// const SubTitle = styled.span`
-//   position: absolute;
-//   top: 10%;
-//   font-size: 1.25rem;
-// `;
+const SubTitle = styled.span`
+  position: absolute;
+  top: 10%;
+  font-size: 1.25rem;
+`;
 
-// const Button = styled.button``;
+const Button = styled.button``;
 
-// const HeaderImage = styled.img`
-//   width: 100%;
-//   height: 31.25rem;
-//   margin-top: 3.8%;
-//   object-fit: cover;
-// `;
+const HeaderImage = styled.img`
+  width: 100%;
+  height: 31.25rem;
+  margin-top: 3.8%;
+  object-fit: cover;
+`;
 
-// const Header = () => {
-//   return (
-//     <Container>
-//       <TitleWrapper>
-//         <SubTitle>TECH</SubTitle>
-//         <Title>BLOG</Title>
-//       </TitleWrapper>
-//       <HeaderImage
-//         src="https://images.pexels.com/photos/257897/pexels-photo-257897.jpeg"
-//         alt="headerImg"
-//       />
-//     </Container>
-//   );
-// };
+const Header = () => {
+  return (
+    <Container>
+      <TitleWrapper>
+        <SubTitle>TECH</SubTitle>
+        <Title>BLOG</Title>
+      </TitleWrapper>
+      <HeaderImage
+        src="https://images.pexels.com/photos/257897/pexels-photo-257897.jpeg"
+        alt="headerImg"
+      />
+    </Container>
+  );
+};
 
-// export default Header;
+export default Header;
