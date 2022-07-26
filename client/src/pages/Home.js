ca7b32601b69ea84b10981fb74146dfae81eddd2
Maurice <mauricenganga41@gmail.com>
2022-07-26 13:04:45 +0300

    Create a apis directory and add a dbjson file to act as our server

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import Header from "../components/Header";
 import Posts from "../components/Posts";
 import Sidebar from "../components/Sidebar";
+import post from "../apis";
 
 const Container = styled.article`
   display: flex;
